d530d9c9429fea19afd91a31a273dfe0cf356c28
vq4 <99613010+vq4@users.noreply.github.com>
2022-02-14 11:20:41 +0900

    Update a.js

--- a.js
+++ a.js
@@ -1,19 +1,19 @@
-surukoto = new Array(
+surukoto1 = new Array(
 "to get enough sleep",	"to solve the problem",	"to swim",	"to go to university",	"to book a hotel online",	"to stay here",	"to decide what to do",	"to eat",	"to travel around the world",	"to come to tomorrow's party",	"to study abroad",	"to save a seat"
 );	
-keiyousi = new Array(
+keiyousi1 = new Array(
 "important",	"impossible",	"amazing",	"possible",	"easy",	"difficult"
 );	
 
-keilen = keiyousi.length;
-surulen = surukoto.length;
+keilen1 = keiyousi1.length;
+surulen1 = surukoto1.length;
 
-keiran = Math.floor(Math.random()*keiyousi.length);
-sururan = Math.floor(Math.random()*surukoto.length);
-keiyou = keiyousi[keiran];
-suru = surukoto[sururan];
-document.write("<p>C 次の問い(<b>問１～３</b>)において、語句を並び替えて文を完成させよ。</p><p>"+suru+" is "+keiyou+".</p>");
+keiran1 = Math.floor(Math.random()*keiyousi1.length);
+sururan1 = Math.floor(Math.random()*surukoto1.length);
+keiyou1 = keiyousi1[keiran1];
+suru1 = surukoto1[sururan1];
+document.write("<p>C 次の問い(<b>問１～３</b>)において、語句を並び替えて文を完成させよ。</p><p>"+suru1+" is "+keiyou1+".</p>");
 
 function myQues(){	
-alert("It is "+keiyou+" "+suru+".");	
+alert("It is "+keiyou1+" "+suru1+".");	
 }	
