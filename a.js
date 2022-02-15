f2bd3618db6dfb37c58110ecd81bffc51b0633e8
vq4 <99613010+vq4@users.noreply.github.com>
2022-02-15 18:48:05 +0900

    Update a.js

--- a.js
+++ a.js
@@ -1,5 +1,4 @@
 mon = Math.floor(Math.random() * 1);
-If (mon > 0){
 surukoto1 = new Array(
 "to get enough sleep",	"to solve the problem",	"to swim",	"to go to university",	"to book a hotel online",	"to stay here",	"to decide what to do",	"to eat",	"to travel around the world",	"to come to tomorrow's party",	"to study abroad",	"to save a seat"
 );	
@@ -15,10 +14,7 @@ sururan1 = Math.floor(Math.random()*surukoto1.length);
 keiyou1 = keiyousi1[keiran1];
 suru1 = surukoto1[sururan1];
 writ = "<p>C 次の問い(<b>問１～３</b>)において、語句を並び替えて文を完成させよ。</p><p>"+suru1+" is "+keiyou1+mon+".</p>"
-}
-else{
-writ = "1"
-}
+
 document.write(writ);
 function myQues(){	
 alert("It is "+keiyou1+" "+suru1+".");	
