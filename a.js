50bd9e0f61487729090ef9d1aef0ba0955be6e9e
vq4 <99613010+vq4@users.noreply.github.com>
2022-02-16 23:01:03 +0900

    Update a.js

--- a.js
+++ a.js
@@ -22,9 +22,10 @@ keiyou1 = keiyousi1[keiran1];
 suru1 = surukoto1[sururan1];
 keiy1 = keiyaku1[keiran1];
 suruy1 = suruyaku1[sururan1];
-writ = "<p>181 次の問いにおいて、同じ意味となるように語句を書き換えて文を完成させよ。</p><p>"+suru1+" is "+keiyou1+".</p>"
-kotae = "It is "+keiyou1+" "+suru1+". ("+suruy1+"は"+keiy1+")"
-}if (mon == "1"){
+writ = "<p>181 次の問いにおいて、同じ意味となるように語句を書き換えて文を完成させよ。</p><p>"+suru1+" is "+keiyou1+".</p>";
+kotae = "It is "+keiyou1+" "+suru1+". ("+suruy1+"は"+keiy1+")";
+}
+if (mon == "1"){
 surukoto1 = new Array(
 "to get enough sleep",	"to solve the problem",	"to swim",	"to go to university",	"to book a hotel online",	"to stay here",	"to decide what to do",	"to eat",	"to travel around the world",	"to come to tomorrow's party",	"to study abroad",	"to save a seat"
 );	
@@ -46,8 +47,8 @@ keiyou1 = keiyousi1[keiran1];
 suru1 = surukoto1[sururan1];
 keiy1 = keiyaku1[keiran1];
 suruy1 = suruyaku1[sururan1];
-writ = "<p>181 次の問いにおいて、同じ意味となるように語句を書き換えて文を完成させよ。</p><p>"+suru1+" is "+keiyou1+".</p>"
-kotae = "It is "+keiyou1+" "+suru1+". ("+suruy1+"は"+keiy1+")"
+writ = "<p>181 次の問いにおいて、同じ意味となるように語句を書き換えて文を完成させよ。</p><p>"+suru1+" is "+keiyou1+".</p>";
+kotae = "It is "+keiyou1+" "+suru1+". ("+suruy1+"は"+keiy1+")";
 }
 document.write(writ);
 function myQues(){	
